87316000ed26bb45a985f817079d31e61dd1f048
PhapNguyen <vietphappro1@gmail.com>
2023-12-07 15:13:40 +0700

    Cart-CartItemController

---
+++ Selling/src/main/java/orishop/services/CartServiceImpl.java
@@ -0,0 +1,20 @@
+package orishop.services;
+
+
+import orishop.DAO.ICartDAO;
+import orishop.DAO.CartDAOImpl;
+import orishop.models.CartModels;
+
+public class CartServiceImpl implements ICartService{
+	ICartDAO cartDAO = new CartDAOImpl();
+	
+	@Override
+	public CartModels findCartId(int cartId) {
+		return cartDAO.findCartId(cartId);
+	}
+
+	@Override
+	public CartModels findCustomerId(int customerId) {
+		return cartDAO.findCustomerId(customerId);
+	}
+}
