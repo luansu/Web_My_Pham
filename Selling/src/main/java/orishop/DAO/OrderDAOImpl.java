ee875b560f5d3be96deaaebfd06e2376aa13ffe9
PhapNguyen <vietphappro1@gmail.com>
2023-12-10 22:05:49 +0700

    Sửa lỗi orderDAO

--- Selling/src/main/java/orishop/DAO/OrderDAOImpl.java
+++ Selling/src/main/java/orishop/DAO/OrderDAOImpl.java
@@ -11,22 +11,25 @@ import orishop.models.OrdersModels;
 
 public class OrderDAOImpl implements IOrderDAO{
 
+	
+	Connection conn = null;
+	PreparedStatement ps = null;
+	ResultSet rs = null;
 	@Override
 	public List<OrdersModels> findAllOrders() {
 		List<OrdersModels> listOrder = new ArrayList<OrdersModels>();
 		String sql = "SELECT * FROM ORDERS";
 		try {
-			new DBConnectionSQLServer();
 			conn = DBConnectionSQLServer.getConnectionW();
 			ps = conn.prepareStatement(sql);
 			rs = ps.executeQuery();
 			while (rs.next()) {
 				OrdersModels orders = new OrdersModels();
-				orders.setOrderId(rs.getInt("orderId"));
+				orders.setOrderID(rs.getInt("orderId"));
 				orders.setOrderValue(rs.getFloat("orderValue"));
 				orders.setOrderDate(rs.getDate("orderDate"));
-				orders.setCartId(rs.getInt("cartId"));
-				orders.setCustomerId(rs.getInt("customerId"));
+				orders.setCartID(rs.getInt("cartId"));
+				orders.setCustomerID(rs.getInt("customerId"));
 				orders.setPaymentStatus(rs.getString("paymentStatus"));
 				orders.setOrderStatus(rs.getString("orderStatus"));
 				orders.setPaymentMethod(rs.getString("paymentMethod"));
