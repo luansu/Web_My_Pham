bc2de7bb4c5f998840ea85f8090b657fc0d0d88a
21110512@st.hcmute.edu.vn <21110512@st.hcmute.edu.vn>
2023-12-13 14:30:57 +0700

    hoan thien product

--- Selling/src/main/java/orishop/controllers/seller/SellerHomeControllers.java
+++ Selling/src/main/java/orishop/controllers/seller/SellerHomeControllers.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.beanutils.BeanUtils;
 
 import orishop.models.CategoryModels;
-import orishop.models.OrdersModels;
 import orishop.models.ProductModels;
 import orishop.services.CategoryServiceImp;
 import orishop.services.CustomerServiceImp;
@@ -27,9 +26,10 @@ import orishop.services.ICustomerService;
 import orishop.services.IEmployeeService;
 import orishop.services.IOrderService;
 
-
-@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 10, maxFileSize = 1024 * 1025 * 50, maxRequestSize = 1024 * 1024* 50)
-@WebServlet(urlPatterns = {"/seller/home", "/seller/listorder", "/seller/detailorder", "/seller/updateorderSeller", "/seller/listproduct", "/seller/insertpro", "/seller/updatepro", "/seller/deletepro"})
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 10, maxFileSize = 1024 * 1025 * 50, maxRequestSize = 1024 * 1024
+		* 50)
+@WebServlet(urlPatterns = { "/seller/home", "/seller/listproduct", "/seller/insertpro", "/seller/updatepro",
+		"/seller/deletepro" })
 
 public class SellerHomeControllers extends HttpServlet {
 	ICategoryService cateService = new CategoryServiceImp();
@@ -38,21 +38,20 @@ public class SellerHomeControllers extends HttpServlet {
 	IProductService proService = new ProductServiceImp();
 	IOrderService orderService = new OrderServiceImpl();
 	private static final long serialVersionUID = 1L;
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String url = req.getRequestURI();
-		if(url.contains("seller/home")) {
+		if (url.contains("seller/home")) {
 			double totalPrice = orderService.totalPriceProductSell();
 			int orderrequest = orderService.countOrderRequest();
-			
+
 			req.setAttribute("earningmonthly", totalPrice);
 			req.setAttribute("earningannual", totalPrice);
 			req.setAttribute("orderrequest", orderrequest);
 			RequestDispatcher rd = req.getRequestDispatcher("/views/seller/revenue.jsp");
 			rd.forward(req, resp);
-		}
-		else if (url.contains("listproduct")) {
+		} else if (url.contains("listproduct")) {
 			List<ProductModels> list = proService.findAllProduct();
 			List<CategoryModels> listcate = cateService.findAllCategory();
 			req.setAttribute("listcate", listcate);
@@ -60,63 +59,37 @@ public class SellerHomeControllers extends HttpServlet {
 
 			RequestDispatcher rd = req.getRequestDispatcher("/views/seller/listproduct.jsp");
 			rd.forward(req, resp);
-		}  else if (url.contains("insertpro")) {
+		} else if (url.contains("insertpro")) {
 			List<CategoryModels> listcate = cateService.findAllCategory();
 			req.setAttribute("listcate", listcate);
 
-			RequestDispatcher rd = req.getRequestDispatcher("views/seller/listproduct.jsp");
+			RequestDispatcher rd = req.getRequestDispatcher("/views/seller/addproduct.jsp");
 			rd.forward(req, resp);
-		}
-		else if (url.contains("updatepro")) {
+		} else if (url.contains("updatepro")) {
 			List<CategoryModels> listcate = cateService.findAllCategory();
 			req.setAttribute("listcate", listcate);
-			ProductModels product = proService.findOne(req.getParameter("id"));
+			ProductModels product = proService.findOne(Integer.parseInt(req.getParameter("id")));
 			req.setAttribute("product", product);
 
-			RequestDispatcher rd = req.getRequestDispatcher("views/seller/listproduct.jsp");
+			RequestDispatcher rd = req.getRequestDispatcher("/views/seller/updateproduct.jsp");
 			rd.forward(req, resp);
-		}
-		else if (url.contains("deletepro")) {
+		} else if (url.contains("deletepro")) {
 			deleteProduct(req, resp);
 		}
-		else if (url.contains("listorder")) {
-			List<OrdersModels> listorder = orderService.findAllOrders();
-			req.setAttribute("listorder", listorder);
-			RequestDispatcher rd = req.getRequestDispatcher("/views/seller/listorder.jsp");
-			rd.forward(req, resp);
-		}
-		else if (url.contains("detailorder")) {
-			int orderid = Integer.parseInt(req.getParameter("orderid"));
-			List<OrdersModels> detailorder = orderService.findOrderID(orderid);
-			req.setAttribute("detailorder", detailorder);
-			RequestDispatcher rd = req.getRequestDispatcher("/views/seller/detailorder.jsp");
-			rd.forward(req, resp);
-		}
-	}	
+	}
+
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String url = req.getRequestURI().toString();
 		if (url.contains("insertpro")) {
 			addProduct(req, resp);
-		}
-		else if (url.contains("updatepro")) {
+		} else if (url.contains("updatepro")) {
 			UpdateProduct(req, resp);
-		}
-		else if (url.contains("updateorderSeller")) {
-			updateOrder(req, resp);
 		}
 	}
 
-	private void updateOrder(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
-		String orderStatus = req.getParameter("orderStatus");
-		int orderId = Integer.parseInt(req.getParameter("orderId"));
-		orderService.UpdateOrderID(orderId, orderStatus);
-		RequestDispatcher rd = req.getRequestDispatcher("/views/seller/listorder.jsp");
-		rd.forward(req, resp);
-	}
-	
 	private void deleteProduct(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		String id = (req.getParameter("id"));
+		int id = Integer.parseInt(req.getParameter("id"));
 		ProductModels product = proService.findOne(id);
 		try {
 			proService.deleteProduct(product);
@@ -125,60 +98,57 @@ public class SellerHomeControllers extends HttpServlet {
 			e.printStackTrace();
 			req.setAttribute("error", "That bai");
 		}
-		RequestDispatcher rd = req.getRequestDispatcher("listproduct");
+		RequestDispatcher rd = req.getRequestDispatcher("/seller/listproduct");
 		rd.forward(req, resp);
 	}
-	
+
 	private void addProduct(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		
+
 		req.setCharacterEncoding("UTF-8");
 		resp.setCharacterEncoding("UTF-8");
-		
+
 		ProductModels model = new ProductModels();
 		try {
 			// lay du lieu tu jsp bang beanutils
 			BeanUtils.populate(model, req.getParameterMap());
-			model.setCategory(cateService.findOne(model.getCategoryId())); 
+			model.setCategory(cateService.findOne(model.getCategoryId()));
 			proService.insertProduct(model);
-			//thông báo kết quả
+			// thông báo kết quả
 			req.setAttribute("product", model);
-			req.setAttribute("message","Add successful");
-			
+			req.setAttribute("message", "Add successful");
+
 		} catch (Exception e) {
 			e.printStackTrace();
-			req.setAttribute("error","Add fails");
+			req.setAttribute("error", "Add fails");
 		}
-		
+
 		resp.sendRedirect(req.getContextPath() + "/listproduct");
 
-		
 	}
-	
+
 	private void UpdateProduct(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		
+
 		req.setCharacterEncoding("UTF-8");
 		resp.setCharacterEncoding("UTF-8");
-		
-		
+
 		ProductModels model = new ProductModels();
 		try {
 			// lay du lieu tu jsp bang beanutils
 			BeanUtils.populate(model, req.getParameterMap());
+
 			// Xử lý truong image
-			
-			//model.setCategory(catService.findOne(model.getCategoryID())); 
+
+			model.setCategory(cateService.findOne(model.getCategoryId()));
 			proService.updateProduct(model);
-			//thông báo kết quả
+			// thông báo kết quả
 			req.setAttribute("product", model);
-			req.setAttribute("message","Update successful");
-			
-			
+			req.setAttribute("message", "Update successful");
+
 		} catch (Exception e) {
 			e.printStackTrace();
-			req.setAttribute("error","Update fails");
+			req.setAttribute("error", "Update fails");
 		}
-		
-		resp.sendRedirect(req.getContextPath() + "/listproduct");	
+
+		resp.sendRedirect(req.getContextPath() + "/seller/listproduct");
 	}
 }
-	
